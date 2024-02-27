2f5624df3e818bb5029abd37b0209eb25b53c839
Kevyn Fyleyssant <kevyn.fyleyssant@gmail.com>
2024-02-27 23:13:38 +0100

    feat: improve link style

--- components/base/Link.vue
+++ components/base/Link.vue
@@ -7,7 +7,8 @@ defineProps<{
 </script>
 
 <template>
-  <NuxtLink :to="to" class="text-light transition-colors border-b border-b-accent hover:text-accent">
+  <NuxtLink :to="to" class="group relative text-light transition-colors hover:text-accent">
     <slot />
+    <div class="absolute inset-x-0 top-full mx-auto h-[1px] w-2 bg-accent transition-all group-hover:w-full" />
   </NuxtLink>
 </template>
