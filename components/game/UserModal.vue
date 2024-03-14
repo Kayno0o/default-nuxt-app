e5d0d0bda4f71bdcb7eab850c20f2eb7d0ebb831
Kaynooo <kevyn.fyleyssant@gmail.com>
2024-03-14 01:14:46 +0100

    feat: remove dump

--- components/game/UserModal.vue
+++ components/game/UserModal.vue
@@ -17,7 +17,6 @@ function onClose() {
 
 <template>
   <Modal :model-value="!user.username" persistent>
-    {{ user }}
     <form class="flex flex-col items-center gap-4" @submit.stop.prevent="onClose">
       <BaseH3>Choose a username</BaseH3>
       <BaseInput id="ws-username" v-model="usernameRef" label="username" />
