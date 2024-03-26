37b4c9dc2f6de953c9ce5fe1c0ed330f39dd4651
Kaynooo <kevyn.fyleyssant@gmail.com>
2024-03-27 00:08:12 +0100

    fix: reactively update todolist content

--- pages/todolist/index.vue
+++ pages/todolist/index.vue
@@ -21,7 +21,7 @@ function setChecked(todo: Todolist, checked: boolean) {
   })
 }
 
-function updateTodo() {
+function updateTodo(todo: Todolist) {
   if (!editTodo.value)
     return
 
@@ -32,6 +32,8 @@ function updateTodo() {
     errorMessage: 'Error while updating todo',
     successMessage: 'Successfully edited todo',
     onSuccess: () => {
+      if (editTodo.value)
+        todo.content = editTodo.value.content
       editTodo.value = null
     },
   })
@@ -72,29 +74,30 @@ function addTodo() {
   <div v-if="user" class="text-center">
     <BaseH1>Todolists</BaseH1>
 
-    <div class="grid grid-cols-3 gap-x-8 gap-y-4">
-      <div v-for="todo in data" :key="todo.id" class="flex justify-between items-center card">
+    <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-4">
+      <div v-for="todo in data" :key="todo.id" class="flex gap-2 justify-between items-center card">
         <BaseCheckbox
           :id="`todo-check-${todo.id}`"
           :model-value="todo.checked"
           :label="editTodo?.id === todo.id ? '' : todo.content"
           @update:model-value="val => setChecked(todo, val)"
         />
-        <BaseInput
-          v-if="editTodo?.id === todo.id"
-          :id="`todo-content-${todo.id}`"
-          v-model="editTodo.content"
-          small
-        />
 
         <div v-if="editTodo?.id !== todo.id" class="flex gap-2">
           <Click class="ph:pencil text-accent" @click="editTodo = { ...todo }" />
           <Click class="ph:trash text-red-5" @click="deleteTodo(todo)" />
         </div>
-        <div v-else class="flex gap-2">
-          <Click class="ph:check-bold text-accent" @click="updateTodo" />
+        <form v-else class="flex gap-2 items-center w-full" @submit.prevent="updateTodo(todo)">
+          <BaseInput
+            v-if="editTodo?.id === todo.id"
+            :id="`todo-content-${todo.id}`"
+            v-model="editTodo.content"
+            small
+            class="w-full"
+          />
+          <Click is="button" type="submit" class="ph:check-bold text-accent" />
           <Click class="ph:x text-red-5" @click="editTodo = null" />
-        </div>
+        </form>
       </div>
     </div>
 
